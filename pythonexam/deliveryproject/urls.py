5771e40cd5b3e5cf73ddc1149993e1ad0f8c53ca
Rami Kouraychi <ramikouraychi@gmail.com>
2026-04-12 16:31:55 +0100

    urls.py

---
+++ pythonexam/deliveryproject/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.delivery_list, name='delivery_list'),
+    path('add/', views.delivery_create, name='delivery_add'),
+    path('update/<int:id>/', views.delivery_update, name='delivery_update'),
+    path('delete/<int:id>/', views.delivery_delete, name='delivery_delete'),
+]
--- pythonexam/pythonexam/urls.py
+++ pythonexam/pythonexam/urls.py
@@ -1,9 +1,23 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for pythonexam project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.delivery_list, name='delivery_list'),
-    path('add/', views.delivery_create, name='delivery_add'),
-    path('update/<int:id>/', views.delivery_update, name='delivery_update'),
-    path('delete/<int:id>/', views.delivery_delete, name='delivery_delete'),
+    path('admin/', admin.site.urls),
+    path('', include('livraison.urls'))
 ]
