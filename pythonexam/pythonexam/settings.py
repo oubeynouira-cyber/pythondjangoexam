28a6b677aa1ec6a9541304ee15bda1889d24d768
oubeynouira-cyber <oubey.nouira@polytechnicien.tn>
2026-04-12 15:52:52 +0100

    fix 1.0

--- pythonexam/pythonexam/settings.py
+++ pythonexam/pythonexam/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ff&n1*&^5yy$wfna4!lb=dkcq9f_$eq3-1c8vc6@6eqwr(ggaq'
+SECRET_KEY = 'django-insecure-=glkgzm%sa)1@&0((rqjed2j=!-h405ybsk&04l#6c*wwk24%f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
